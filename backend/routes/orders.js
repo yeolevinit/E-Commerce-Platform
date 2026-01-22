0bbb2ae13abdf3330f07f154e5638dd37ee7e9e9
yeolevinit <yeolevinit24@gmail.com>
2026-01-22 23:45:58 +0530

    some erorr resolved

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { createCheckoutSession, verifyPayment, getMyOrders } from '../controllers/orderController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { protect } from '../middleware/auth.js';
 
 const router = express.Router();
 
